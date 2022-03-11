0fd48db3d707ab14a6476cc69df7b15822b051b4
pahirdinhamut <69786935+pahirdinhamut@users.noreply.github.com>
2022-03-11 17:11:01 +0100

    added buttom & props

---
+++ component/Buttom/styles.js
@@ -0,0 +1,28 @@
+import {StyleSheet} from "react-native";
+
+const styles = StyleSheet.create(
+    {
+        container:{
+            width:'100%',
+            padding:10
+
+        },
+        text:{
+            fontSize:12,
+            fontWeight:'500',
+            textTransform:'uppercase'
+        },
+        buttom:{
+            height:40,
+            justifyContent:'center',
+            alignItems:'center',
+            borderRadius:40,
+
+        }
+    }
+
+
+)
+
+
+export default styles;
--- component/CarItem/index.js
+++ component/CarItem/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import styles from "./style"
+import Buttom from "../Buttom";
 
 import {StyleSheet, View, Text, ImageBackground} from "react-native";
 
@@ -14,6 +15,16 @@ const CarItem = ()=>{
                 <Text style={styles.title}>Model S</Text>
                 <Text style={styles.subtitle}>Strating at $69,420</Text>
             </View>
+            <Buttom
+                type="primary"
+                content={"Costum Order"}
+                onPress={()=>{console.warn("button press")}}
+            />
+            <Buttom
+                type="secondary"
+                content={"Existing Invertery"}
+                onPress={()=>{console.warn("button press 2")}}
+            />
         </View>
     )
 }
